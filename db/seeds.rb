49c6358df002bcaddb0497bce2ed70586bef95b8
Vasily Ponomarev <vasil.ponomarev@gmail.com>
2012-08-12 09:40:11 +0400

    Minor fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seed measurements
+measurements = [ { name: "Google", mtype: "word", url: "http://www.google.com/insights/search/#&date=today%201-m&cmpt=q&q=" },
+                 { name: "Google", mtype: "site", url: "http://www.google.com/insights/search/#&date=today%201-m&cmpt=q&q=" },
+                 { name: "Alexa", mtype: "site", url: "http://www.alexa.com/siteinfo/" } ]
+
+for measurement in measurements
+  Measurement.create!(name: measurement[:name],
+                      mtype: measurement[:mtype],
+                      url:  measurement[:url])
+end
