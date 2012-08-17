873f095722c2e44b99576e10dff1e68adb5eb9cc
Vasily Ponomarev <vasil.ponomarev@gmail.com>
2012-08-17 21:50:43 +0400

    Improved Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,35 @@
+require 'bundler/capistrano'
+
 set :application, "trend-finder-frontend"
-set :domain, "global-trend-finder.com"
 set :user, "globapd6"
+set :domain, "global-trend-finder.com"
 set :use_sudo, false
-
-set :repository, "file:///home/#{user}/repository/#{application}.git"
-set :local_repository, "ssh://#{user}@#{domain}/home/#{user}/repository/#{application}.git"
-
-default_run_options[:pty] = true
 
 set :scm, :git
 set :branch, "master"
+
+set :repository, "file:///home/#{user}/repository/#{application}.git"
+#set :local_repository, "ssh://#{user}@#{domain}/home/#{user}/repository/#{application}.git"
+set :local_repository, "."
+
 set :deploy_to, "/home/#{user}/rails_apps/#{application}"
-set :deploy_via, :export
+set :deploy_via, :remote_cache
 
-role :web, domain
-role :app, domain
-role :db,  domain, :primary => true
+server domain, :app, :web, :db, :primary => true
 
-# if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
-
-namespace :deploy do
-  run_locally("git push bluehost")
+#namespace :deploy do
+  #@run_locally("git push bluehost")
   #task :restart do
     #run "touch #{File.join(current_path,'tmp','restart.txt')}"
-    #run "cd #{current_path} && bundle install"
-    #run "cd #{current_path} && bundle exec rake assets:precompile"
   #end
+#end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
 end
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+after "deploy:restart", "deploy:cleanup"
