2f2d7332a20dcead8d4472de6a143f7a715f65e1
Vasily Ponomarev <vasil.ponomarev@gmail.com>
2012-08-17 09:27:31 +0400

    Improved search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,16 @@
 class SearchController < ApplicationController
   def index
     query = params[:search][:query]
-    if query.length > 3
+    if query.length > 2
       @words = Word.select('*').from('(' +
                  Word.last_measurement
                    .where([ 'measurement_id = ?', Measurement.find_by_mtype_and_name('word', 'Google') ]).to_sql + ') words')
-                   .where([ "name ILIKE ?", "%" + query + "%" ])
+                   .where([ "name ILIKE ? ESCAPE '!'", "%" + query.gsub(/[!%_]/) { |x| '!' + x } + "%"])
                  .order('value DESC')
       @sites = Site.select('*').from('(' +
                  Site.last_measurement
                    .where([ 'measurement_id = ?', Measurement.find_by_mtype_and_name('site', 'Google') ]).to_sql + ') sites')
-                   .where([ "name ILIKE ?", "%" + query + "%" ])
+                   .where([ "name ILIKE ? ESCAPE '!'", "%" + query.gsub(/[!%_]/) { |x| '!' + x } + "%"])
                  .order('value DESC')
     else
       @error = 'Query is too short'
