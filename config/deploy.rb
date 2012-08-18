9e5d90db00100af1f76368303c550db4cbb889a0
Vasily Ponomarev <vasil.ponomarev@gmail.com>
2012-08-18 10:13:58 +0400

    Added comments to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,43 @@
+# Automatic "bundle install" after deploy
 require 'bundler/capistrano'
 
+# Application name
 set :application, "trend-finder-frontend"
+
+# BlueHost SSH user
 set :user, "globapd6"
+
+# App Domain
 set :domain, "global-trend-finder.com"
+
+# We don't need sudo on BlueHost
 set :use_sudo, false
 
+# git is our SCM
 set :scm, :git
+
+# master is our default git branch
 set :branch, "master"
 
-set :repository, "file:///home/#{user}/repository/#{application}.git"
-#set :local_repository, "ssh://#{user}@#{domain}/home/#{user}/repository/#{application}.git"
-set :local_repository, "."
+# Use local git repository
+set :repository, "."
 
+# Checkout, compress and send a local copy
+set :deploy_via, :copy
 set :deploy_to, "/home/#{user}/rails_apps/#{application}"
-set :deploy_via, :remote_cache
 
+# We have all components of the app on the same server
 server domain, :app, :web, :db, :primary => true
-
-#namespace :deploy do
-  #@run_locally("git push bluehost")
-  #task :restart do
-    #run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  #end
-#end
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
+  
+  # Touch tmp/restart.txt to tell Phusion Passenger about new version
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
   end
 end
 
+# Clean-up old releases
 after "deploy:restart", "deploy:cleanup"
