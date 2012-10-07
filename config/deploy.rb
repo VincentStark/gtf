86f16e3c53a53a84966e1ec7a9bda266e3a0db42
Vasily Ponomarev <vasil.ponomarev@gmail.com>
2012-10-07 16:31:06 +0400

    Deployment script fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # Automatic "bundle install" after deploy
-require 'bundler/capistrano'
+require "bundler/capistrano"
 
 # RVM integration
-require 'rvm/capistrano'
+require "rvm/capistrano"
 
 # Assets precompile
-load 'deploy/assets'
+load "deploy/assets"
 
 # Application name
 set :application, "gtf"
@@ -18,6 +18,7 @@ set :domain, "global-trend-finder.com"
 
 # Minor tweaks
 set :use_sudo, false
+set :rvm_ruby_string, '1.9.3'
 set :rvm_install_with_sudo, true
 set :rvm_type, :system
 
@@ -38,7 +39,12 @@ set :deploy_to, "/var/rails/#{application}"
 server domain, :app, :web, :db, :primary => true
 
 # Install rvm before deploy
-before 'deploy:setup', 'rvm:install_rvm'
+before "deploy:setup", "rvm:install_rvm"
+before "deploy:setup", "rvm:install_ruby"
+
+# Use default rvm version
+after "deploy:setup", "deploy:set_rvm_version"
+after "deploy:setup", "deploy:fix_setup_permissions"
 
 # Unicorn setup
 set :rails_env, :production
@@ -49,27 +55,37 @@ set :su_rails, "sudo -u rails"
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
-    # Normalize permissions
-    run "mkdir #{current_path}/tmp"
-    run "#{sudo} chgrp -R rails #{current_path}/tmp"
-    run "#{sudo} chmod 775 #{current_path}/tmp"
-    run "#{sudo} chgrp -R rails #{current_path}/log"
-    run "#{sudo} chmod 775 #{current_path}/log"
-    # Launch unicorn
     run "cd #{current_path} && #{su_rails} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
+
   task :stop, :roles => :app, :except => { :no_release => true } do 
-    run "#{su_rails} kill `cat #{unicorn_pid}`"
+    run "test -f #{unicorn_pid} && #{su_rails} kill `cat #{unicorn_pid}`"
   end
+
   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    run "#{su_rails} kill -s QUIT `cat #{unicorn_pid}`"
+    run "test -f #{unicorn_pid} && #{su_rails} kill -s QUIT `cat #{unicorn_pid}`"
   end
+
   task :reload, :roles => :app, :except => { :no_release => true } do
-    run "#{su_rails} kill -s USR2 `cat #{unicorn_pid}`"
+    run "test -f #{unicorn_pid} && #{su_rails} kill -s USR2 `cat #{unicorn_pid}`"
   end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
     stop
     start
+  end
+  
+  task :set_rvm_version, :roles => :app, :except => { :no_release => true } do
+    run "source /etc/profile.d/rvm.sh && rvm use #{rvm_ruby_string} --default"
+  end
+
+  task :fix_setup_permisssions, :roles => :app, :except => { :no_release => true } do
+    run "#{sudo} chgrp rails #{shared_path}/log"
+    run "#{sudo} chgrp rails #{shared_path}/pids"
+  end
+
+  task :fix_permisssions, :roles => :app, :except => { :no_release => true } do
+    run "#{sudo} chgrp -R rails #{current_path}/tmp"
   end
 end
 
