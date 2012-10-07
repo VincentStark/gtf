7a4fa646cc255dc3ec6a9100dbf4c131d2bf1d4c
Vasily Ponomarev <vasil.ponomarev@gmail.com>
2012-10-07 08:52:39 +0400

    Improvements in deploy

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
 gem 'rails', '3.2.1'
 
@@ -70,6 +70,7 @@ gem 'unicorn'
 
 # Deploy with Capistrano
 gem 'capistrano'
+gem 'rvm-capistrano'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,67 @@
 # Automatic "bundle install" after deploy
 require 'bundler/capistrano'
 
+# RVM integration
+require 'rvm/capistrano'
+
 # Assets precompile
 load 'deploy/assets'
 
 # Application name
-set :application, "global-trend-finder"
+set :application, "gtf"
 
-# BlueHost SSH user
-set :user, "globapd6"
+# Username
+set :user, "ec2-user"
 
 # App Domain
 set :domain, "global-trend-finder.com"
 
-# We don't need sudo on BlueHost
-set :use_sudo, false
+# Minor tweaks
+set :use_sudo, true
+set :rvm_type, :system
 
 # git is our SCM
 set :scm, :git
 
+# Use github repository
+set :repository, "git://github.com/vasily-ponomarev/gtf.git"
+
 # gtf is our default git branch
 set :branch, "gtf"
 
-# Use local git repository
-set :repository, "."
-
-# Checkout, compress and send a local copy
-set :deploy_via, :copy
-set :deploy_to, "/home/#{user}/rails_apps/#{application}"
+# Deploy local copy
+set :deploy_via, :remote_cache
+set :deploy_to, "/var/rails/#{application}"
 
 # We have all components of the app on the same server
 server domain, :app, :web, :db, :primary => true
 
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
+# Install rvm before deploy
+before 'deploy:setup', 'rvm:install_rvm'
 
-  # Touch tmp/restart.txt to tell Phusion Passenger about new version
+# Unicorn setup
+set :rails_env, :production
+set :unicorn_binary, "unicorn_rails"
+set :unicorn_config, "#{current_path}/config/unicorn.conf.rb"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+set :su_rails, "sudo -u rails"
+
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do 
+    run "cd #{current_path} && #{su_rails} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
+  end
+  task :stop, :roles => :app, :except => { :no_release => true } do 
+    run "#{su_rails} kill `cat #{unicorn_pid}`"
+  end
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "#{su_rails} kill -s QUIT `cat #{unicorn_pid}`"
+  end
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "#{su_rails} kill -s USR2 `cat #{unicorn_pid}`"
+  end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+    stop
+    start
   end
 end
 
