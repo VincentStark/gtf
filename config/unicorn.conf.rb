1a20d7ad94996503a3b5a66e0e4ab9203635d238
Vasily Ponomarev <vasil.ponomarev@gmail.com>
2012-10-07 19:50:31 +0400

    Change worker processes from 16 to 4

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,6 +1,6 @@
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 16
+worker_processes 4
 
 #user "rails", "rails"
 @app = "/var/rails/gtf/current"
